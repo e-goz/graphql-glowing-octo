4db36ce018faff0fdc732336c3b48cf0a3ecc4a6
guillaume <egoz67@gmail.com>
2019-01-29 22:53:10 +0100

    First test of querying API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount = () => {
+    client
+      .query({
+        query: gql`
+          {
+            allPeople {
+              id
+              name
+            }
+          }
+        `
+      })
+      .then(result => {
+        console.log(result);
+        this.setState({ data: result.data.allPeople });
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hello world
+        <ul>
+          {this.state.data.map(f => (
+            <li>{f.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
